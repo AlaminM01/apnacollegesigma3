f06bd87e1de3695708bf82ffdafb55f1f14a7980
Alamin Mondal <alaminmondalce@gmail.com>
2026-07-08 10:12:13 +0530

    this day 2 ans

---
+++ day2q2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class day2q2 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in) ;
+       int square = sc.nextInt();
+       int area = square * square;
+       System.out.println(area);
+    }
+}
